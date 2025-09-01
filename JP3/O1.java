59ead6508bbb17ef07fe4e8ac86dd62928120d42
Per Fimreite <per.fimreite@gmail.com>
2025-09-01 18:03:20 +0200

    fixed O1.java's logic

--- JP3/O1.java
+++ JP3/O1.java
@@ -4,18 +4,18 @@ public class O1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Bruttoinntekt: ");
-        int bruttointekt = scanner.nextInt();
+        int bruttoinntekt = scanner.nextInt();
+
+        double trinnskatt = 0;
+
+        if (bruttoinntekt >   217_400) trinnskatt += (      306_050 -   217_401) * 0.017;
+        if (bruttoinntekt >   306_050) trinnskatt += (      697_150 -   306_051) * 0.040;
+        if (bruttoinntekt >   697_150) trinnskatt += (      942_400 -   697_151) * 0.137;
+        if (bruttoinntekt >   942_400) trinnskatt += (    1_410_570 -   942_401) * 0.167;
+        if (bruttoinntekt > 1_410_750) trinnskatt += (bruttoinntekt - 1_410_750) * 0.177;
+
+        System.out.println("Trinnskatt: " + trinnskatt + "kroner");
+
         scanner.close();
-
-        double trinnskatt;
-
-        if (bruttointekt <= 217_400)        trinnskatt =    0;
-        else if (bruttointekt <= 306_050)   trinnskatt =  1.7;
-        else if (bruttointekt <= 697_150)   trinnskatt =  4.0;
-        else if (bruttointekt <= 942_400)   trinnskatt = 13.7;
-        else if (bruttointekt <= 1_410_750) trinnskatt = 16.7;
-        else                                trinnskatt = 17.7;
-
-        System.out.println("Trinnskatt: " + trinnskatt + "%");
     }
 }
